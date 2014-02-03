51559a249fa38dfab8c0bd4ccd56ba6d9ec7332f
Kristian Pulkkinen <kristian.pulkkinen@cs.helsinki.fi>
2014-02-03 09:41:49 +0200

    rspec installoitu

--- Gemfile
+++ Gemfile
@@ -14,6 +14,10 @@ gem 'debugger', group: [:development, :test]
 
 gem 'bcrypt-ruby', '~> 3.1.2'
 
+group :development, :test do
+   gem 'rspec-rails', '~> 2.14.1'
+end
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,5 +1,6 @@
 class BeersController < ApplicationController
 	before_action :set_beer, only: [:show, :edit, :update, :destroy]
+	before_action :set_breweries_and_styles_for_template, only: [:new, :edit, :create]
 
 	# GET /beers
 	# GET /beers.json
@@ -15,8 +16,6 @@ class BeersController < ApplicationController
 	# GET /beers/new
 	def new
 		@beer = Beer.new
-		@breweries = Brewery.all
-		@styles = style_list
 	end
 
 	# GET /beers/1/edit
@@ -33,8 +32,6 @@ class BeersController < ApplicationController
 				format.html { redirect_to beers_path, notice: 'Beer was successfully created.' }
 				format.json { render action: 'show', status: :created, location: @beer }
 			else
-				@styles = style_list
-				@breweries = Brewery.all
 				format.html { render action: 'new' }
 				format.json { render json: @beer.errors, status: :unprocessable_entity }
 			end
@@ -76,7 +73,8 @@ class BeersController < ApplicationController
 			params.require(:beer).permit(:name, :style, :brewery_id)
 		end
 
-		def style_list
-			['Weizen', 'Lager', 'Pale ale', 'IPA', 'Porter']
+		def set_breweries_and_styles_for_template
+			@breweries = Brewery.all
+			@styles = ['Weizen', 'Lager', 'Pale ale', 'IPA', 'Porter']
 		end
 end
