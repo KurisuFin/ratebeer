76ab14bb5a1f14dfcf0089a34821bbb83622e8f7
Kristian Pulkkinen <kristian.pulkkinen@cs.helsinki.fi>
2014-02-11 12:47:12 +0200

    Revert "Nyt toimii Heroku"
    This reverts commit 94e05bc59312fd9321a209e1043747c0716e07d5.

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -9,23 +9,6 @@ class BeermappingApi
 		Rails.cache.fetch(place_id, expires_in: 1.day) { fetch_place(place_id)}
 	end
 
-
-	private
-
-	def self.fetch_places_in(city)
-		url = "http://beermapping.com/webservice/loccity/#{key}/"
-
-		response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
-		places = response.parsed_response['bmp_locations']['location']
-
-		return [] if places.is_a?(Hash) and places['id'].nil?
-
-		places = [places] if places.is_a?(Hash)
-		places.inject([]) do | set, place |
-			set << Place.new(place)
-		end
-	end
-
 	def self.fetch_place(place_id)
 		url = "http://beermapping.com/webservice/locquery/#{key}/"
 
@@ -35,6 +18,32 @@ class BeermappingApi
 		return nil if place_info['id'] == '0'
 
 		Place.new(place_info)
+	end
+
+
+	private
+
+	def self.fetch_places_in(city)
+		url = "http://beermapping.com/webservice/loccity/#{key}/"
+
+		response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+
+#		puts "DERP response: #{response}"
+#		parsed_response = response.parsed_response
+#		puts "HERP parsed_response: #{parsed_response}"
+#		bmp_locations = parsed_response['bmp_locations']
+#		puts "HURP bmp_locations: #{bmp_locations}"
+#		places = bmp_locations['location']
+#		puts "DURP places: #{places}"
+
+		places = response.parsed_response['bmp_locations']['location']
+
+		return [] if places.is_a?(Hash) and places['id'].nil?
+
+		places = [places] if places.is_a?(Hash)
+		places.inject([]) do | set, place |
+			set << Place.new(place)
+		end
 	end
 
 	def self.key
