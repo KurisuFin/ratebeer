aab4e5082dd5c7c8825214b92f95176693fc1ac6
Kristian Pulkkinen <kristian.pulkkinen@cs.helsinki.fi>
2014-02-12 12:52:39 +0200

    Beerin sarake style migratoitu uuden nimiseksi

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212103952) do
+ActiveRecord::Schema.define(version: 20140212104747) do
 
   create_table "beer_clubs", force: true do |t|
     t.string   "name"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20140212103952) do
 
   create_table "beers", force: true do |t|
     t.string   "name"
-    t.string   "style"
+    t.string   "oldstyle_"
     t.integer  "brewery_id"
     t.datetime "created_at"
     t.datetime "updated_at"
